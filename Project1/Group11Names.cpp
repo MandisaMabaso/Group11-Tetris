b037c53d2d371fb6c7558d002794e963a3a712f1
mandisi <212529699@stu.ukzn.ac.za>
2016-04-16 00:51:28 -0700

    t

--- Project1/Group11Names.cpp
+++ Project1/Group11Names.cpp
@@ -2,28 +2,40 @@
 #include <allegro5\allegro_font.h>
 #include <allegro5\allegro_ttf.h>
 #include <allegro5\allegro_native_dialog.h>
+#define ScreenWidth  800
+#define ScreenHeight 600
 
 
 int main(void)
 {
-	ALLEGRO_DISPLAY *display = NULL;
-	display = al_create_display(640, 480);
+	if (!al_init())
+	{
+		al_show_native_message_box(NULL, NULL, NULL, "Could not initialise allegro 5", NULL, NULL);
+		return -1;
+	}
+
+	al_set_new_display_flags(ALLEGRO_WINDOWED);
+	ALLEGRO_DISPLAY *display = al_create_display(ScreenWidth, ScreenHeight);
+	al_set_window_position(display, 200, 100);
+	al_set_window_title(display, "Mandisi Commit");
+
+	if (!display)
+	{
+		al_show_native_message_box(display, "Sample Title", "Displa setting", "Dislay window was no created successfuly", NULL, ALLEGRO_MESSAGEBOX_WARN);
+		return -1;
+	}
 
 	al_init_font_addon();
 	al_init_ttf_addon();
-	//
-	ALLEGRO_FONT *font2 = al_load_font("Arimo-Bold.ttf", 20, 0);
-	al_clear_to_color(al_map_rgb(0, 0, 0));
-	al_draw_text(font2, al_map_rgb(255, 0, 255),50,50,0, "Perfect M. Madondo");
+
+	ALLEGRO_FONT *font = al_load_font("Orbitron Black.tff", 36, NULL);
+
+	al_draw_text(font, al_map_rgb(44, 117, 255), ScreenWidth / 2, ScreenHeight / 2, ALLEGRO_ALIGN_CENTER, "MANDISI");
+
 	al_flip_display();
-	al_draw_text(font2, al_map_rgb(255, 0, 255), 50, 80, 0, "Mlungiseni Sikhakhane");
-	al_flip_display();
-	al_draw_text(font2, al_map_rgb(255, 0, 255), 50, 90, 0, "Mtambo Sibonelo");
-	al_flip_display();
-	al_draw_text(font2, al_map_rgb(255, 0, 255), 50, 100, 0, "Mandisa Mabaso");
-	al_flip_display();
-	al_draw_text(font2, al_map_rgb(255, 0, 255), 50, 100, 0, "Mandisi Shunqukela");
-	al_flip_display();
-	al_rest(5.0);
+	al_rest(10.0);
+	al_destroy_font(font);
+	al_destroy_display(display);
+
 	return 0;
 }
