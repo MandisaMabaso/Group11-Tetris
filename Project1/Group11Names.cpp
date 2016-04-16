1eaaa112cd511b288ceadcdcdfb9ad5eb1e0b0a1
mandisa <sbomabast@gmail.com>
2016-04-16 01:08:07 -0700

    first commit

--- Project1/Group11Names.cpp
+++ Project1/Group11Names.cpp
@@ -17,20 +17,20 @@ int main(void)
 	al_set_new_display_flags(ALLEGRO_WINDOWED);
 	ALLEGRO_DISPLAY *display = al_create_display(ScreenWidth, ScreenHeight);
 	al_set_window_position(display, 200, 100);
-	al_set_window_title(display, "Mandisi Commit");
+	al_set_window_title(display, "display my name");
 
 	if (!display)
 	{
-		al_show_native_message_box(display, "Sample Title", "Displa setting", "Dislay window was no created successfuly", NULL, ALLEGRO_MESSAGEBOX_WARN);
+		al_show_native_message_box(display, "Sample Title", "Display setting", "Display window was not created successfully", NULL, ALLEGRO_MESSAGEBOX_WARN);
 		return -1;
 	}
 
 	al_init_font_addon();
 	al_init_ttf_addon();
 
-	ALLEGRO_FONT *font = al_load_font("Orbitron Black.tff", 36, NULL);
+	ALLEGRO_FONT *font = al_load_font("Orbitron Light.tff", 36, NULL);
 
-	al_draw_text(font, al_map_rgb(44, 117, 255), ScreenWidth / 2, ScreenHeight / 2, ALLEGRO_ALIGN_CENTER, "MANDISI");
+	al_draw_text(font, al_map_rgb(44, 117, 255), ScreenWidth / 2, ScreenHeight / 2, ALLEGRO_ALIGN_LEFT, "Mandisa");
 
 	al_flip_display();
 	al_rest(10.0);
