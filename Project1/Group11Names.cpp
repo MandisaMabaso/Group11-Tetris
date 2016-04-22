b687a8edfc470adf6ef29831fad9549a71eb3e49
mandisi <212529699@stu.ukzn.ac.za>
2016-04-22 03:15:12 -0700

    platform started

--- Project1/Group11Names.cpp
+++ Project1/Group11Names.cpp
@@ -29,7 +29,7 @@ int main(void)
 	al_init_font_addon();
 	al_init_ttf_addon();
 
-	ALLEGRO_FONT *font = al_load_font("Orbitron Light.ttf", 36, NULL);
+	ALLEGRO_FONT *font = al_load_font("orbitron-black.ttf", 36, NULL);
 
 	al_draw_text(font, al_map_rgb(44, 117, 255), ScreenWidth / 2, ScreenHeight / 2, ALLEGRO_ALIGN_LEFT, "Mandisi");
 
@@ -39,4 +39,35 @@ int main(void)
 	al_destroy_display(display);
 
 	return 0;
+
+	// platform dimensions definition for the game
+#define gridHeight 16     // rows
+#define gridWidth 10      //columns
+
+	//This is still to be implemented as a class, so that everyone could just use the ".h" file for this
+	//meanwhile this serves the purpose of the platform creation of the game.Dimensions shall reamin as they are!!, so feel free to
+	//incorporate them in your sections.>>>Peace!
+
+	// int const gridHeight = 16;
+	//int const gridWidth = 10;
+
+	int tetrisPlatform[gridHeight][gridWidth] = {			//Here I'm just defining the 16 by 10 matrix to map out the platform
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+	};
+
 }
